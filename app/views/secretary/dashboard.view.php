434e975b8ff0fbf13d3f1e4916e09f7e7f951c4a
Praveen Madushan <prvnmadushan@gmail.com>
2024-11-07 02:30:43 +0530

    sidebar component added

--- app/views/secretary/dashboard.view.php
+++ app/views/secretary/dashboard.view.php
@@ -1,4 +1,4 @@
-<html>
+
 <head>
     <link rel="stylesheet" href="<?=ROOT?>/assets/css/secretary/dashboard.style.css">
     
@@ -10,13 +10,16 @@
     $notification="notification"; //use notification-dot if there's a notification
     $menuItems = [ "home" => ROOT."/secretary",$memocart => ROOT."/secretary/memocart", $notification => ROOT."/secretary/notifications", "profile" => ROOT."/secretary/viewprofile"]; //pass the menu items here (key is the name of the page, value is the url)
     require_once("../app/views/components/navbar.php"); //call the navbar component
-    $showAddEvents = true;
+    $showAddEvents = true; //set to true if the user have permission to add events
     ?>
-    <div>
-    <?php  
-    require_once("../app/views/components/Calender/calandar-view.php"); //call the calander component
+    <?php
+    $notificationsArr =[
+        "Reminder: Meeting scheduled for tomorrow at 3 PM.",
+        "Update: Minutes from yesterday's meeting are now available.",
+        "Memo Added: A new memo has been attached to this week’s meeting.",
+        "Backup Complete: Meeting records were successfully backed up today.",
+    ]; //pass the notifications here
+    $name = "John Doe"; //pass the name of the user here  
+    require_once("../app/views/components/dashboard-sidebar.php"); //call the dashboard sidebar component
     ?>
-    </div>
 </body>
-
-</html>
