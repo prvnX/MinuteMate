4ccd07a2079f1fc2da4d2e930ca7c5ebf381364d
Sandaruwani11 <gaweshika11@gmail.com>
2024-11-08 11:03:06 +0530

    Applied stashed changes

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,7 +1,20 @@
 <?php
 class Admin extends Controller {
     public function index() {
-        echo "This is admin Controller";
-              $this->view("admin/dashboard");
+        $this->view("admin/dashboard");
+    }
+
+    public function search() {
+        $this->view("404");
+    }
+
+    public function viewpendingRequests() {
+        $this->view("admin/viewpendingRequests");
+    }
+    public function viewMembers() {
+        $this->view("admin/viewMembers");
+    }
+    public function RemoveMembers() {
+        $this->view("admin/RemoveMembers");
     }
 }
--- app/views/admin/dashboard.view.php
+++ app/views/admin/dashboard.view.php
@@ -1,1 +1,62 @@
-<h1>Hello I'm Admin Dashboard</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="<?=ROOT?>/assets/css/admin/admin.style.css">
+</head>
+
+<body>
+    
+
+<div class="navbar">
+<?php
+    
+    $user="admin";
+    $notification="notification"; //use notification-dot if there's a notification
+    $menuItems = [ "home" => ROOT."/admin", $notification => ROOT."/admin/notifications", "profile" => ROOT."/admin/viewprofile"]; //pass the menu items here (key is the name of the page, value is the url)
+    require_once("../app/views/components/navbar.php"); //call the navbar component
+    $showAddEvents = false; 
+   ?>
+</div>
+
+  <div class="container">
+    <div class="card">
+      <img src="<?=ROOT?>/assets/images/View Pending Member Request.jpeg" alt="View Pending Member Request">
+      <a href="<?=ROOT?>/admin/viewpendingRequests">
+      <button>View Pending Member Request</button>
+      </a>
+    </div>
+
+    <div class="card">
+      <img src="<?=ROOT?>/assets/images/View Members.jpeg" alt="View Members">
+      <a href="<?=ROOT?>/admin/viewMembers">
+      <button>View Members</button>
+    </a>
+    </div>
+
+    <div class="card">
+      <img src="<?=ROOT?>/assets/images/Remove Members.jpeg" alt="Remove Members">
+      <a href="<?=ROOT?>/admin/RemoveMembers">
+      <button>Remove Members</button>
+    </a>
+    </div>
+    </div>
+    
+    <div class="aside">
+  <?php
+    $notificationsArr =[
+        "Requests: 3 Requests are pending.",
+        "Reminder: Meeting shceduled for tommorow at a 3 pm ",
+       
+    ]; //pass the notifications here
+    $name = "John Doe"; //pass the name of the user here  
+    require_once("../app/views/components/dashboard-sidebar.php"); //call the dashboard sidebar component
+    ?>
+ </div>
+
+    
+</body>
+
+</html>
