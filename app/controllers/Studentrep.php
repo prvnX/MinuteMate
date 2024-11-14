0f9dbe0a445bedc649305d436b744247f478dd81
Praveen Madushan <prvnmadushan@gmail.com>
2024-11-15 02:48:53 +0530

    add creatememo feature to lec and student

--- app/controllers/Studentrep.php
+++ app/controllers/Studentrep.php
@@ -5,4 +5,22 @@ class Studentrep extends Controller {
         $this->view("studentrep/dashboard");
 
     }
+    public function search() {
+        echo "search";
+        $this->view("404");
+    }
+    public function entermemo() {
+        $this->view("studentrep/entermemo");
+    }
+    public function submitmemo() {
+        $memosuccess = true;
+        $memoid = 1;
+        if($memosuccess) {
+            $this->view("showsuccessmemo",["user"=>"studentrep","memoid"=>$memoid]);
+        }
+        else {
+            $this->view("showunsuccessmemo",["user"=>"studentrep"]);
+        }
+    }
+
 }
