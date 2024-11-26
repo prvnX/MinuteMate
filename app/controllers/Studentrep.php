78b74f746b71dd34309e8dd9077a9dafce467f6c
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2024-11-26 16:23:25 +0530

    requests change create crud done

--- app/controllers/Studentrep.php
+++ app/controllers/Studentrep.php
@@ -21,6 +21,7 @@ class Studentrep extends BaseController {
         else {
             $this->view("showunsuccessmemo",["user"=>"studentrep"]);
         }
+
     }
 
     public function viewminutes() {
@@ -39,7 +40,9 @@ class Studentrep extends BaseController {
             "profile" => ROOT."/studentrep/viewprofile"
         ];
         $this->view("notifications",[ "user" => $user, "menuItems" => $menuItems,"notification" => $notification]);
+
     }
+
     public function viewprofile(){
         $this->view("studentrep/viewprofile");
     }
@@ -58,14 +61,13 @@ class Studentrep extends BaseController {
     
         // Handle POST request
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $field = $_POST['field'] ?? null;
-            $newValue = $_POST['newValue'] ?? null;
-    
-            if (!empty($field) && !empty($newValue)) {
-                $responseStatus = "success";
-            } else {
-                $responseStatus = "failure";
-            }
+            $field = $_POST['field'] ?? [];
+            $newValue = $_POST['newValue'] ?? [];
+            $message = $_POST['message'] ?? "Message not provided";
+            $requestchange = new User_edit_requests();
+            $requestchange->addUserRequest($field, $newValue, $message);
+            $responseStatus = "success";
+
         }
     
         // Pass responseStatus to the view
