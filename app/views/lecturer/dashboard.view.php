d18849efe27e717c4e53a7d66140d8e2f77f5d9e
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2024-11-28 16:58:25 +0530

    lectrer memo and minute report

--- app/views/lecturer/dashboard.view.php
+++ app/views/lecturer/dashboard.view.php
@@ -53,17 +53,18 @@
     </div>
 
     <div class="card">
-      <img src="<?=ROOT?>/assets/images/view memo report.jpeg" alt="View Memo Reports">
-      <a href="<?=ROOT?>/lecturer/viewmemoreports">
-      <button class = "card button">View Memo Reports</button>
-      </a>
-    </div>
 
-    <div class="card">
+<img src="<?=ROOT?>/assets/images/view memo report.jpeg" alt="View Memo Reports">
+  <a href="<?=ROOT?>/lecturer/selectmemo">
+<button class="card-button">View Memo Reports</button>
+  </a>
+</div>
+
+<div class="card">
       <img src="<?=ROOT?>/assets/images/view minute report.png" alt="View Minute Report">
-      <a href="<?=ROOT?>/lecturer/viewminutereports">
-      <button class = "card button">View Minute Report</button>
-      </a>
+        <a href="<?=ROOT?>/lecturer/viewminutereports">
+      <button class="card-button">View Minute Report</button>
+        </a>
     </div>
   </div>
 
