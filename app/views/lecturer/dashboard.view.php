cf76257ec41cdeceb7e59704551ca1cd8708bb52
Nuwan Chanuka <140190472+Nuwanchanuka@users.noreply.github.com>
2025-02-28 20:56:57 +0530

    last lecturer update

--- app/views/lecturer/dashboard.view.php
+++ app/views/lecturer/dashboard.view.php
@@ -14,7 +14,7 @@
     $notification="notification"; //use notification-dot if there's a notification
     $menuItems = [ "home" => ROOT."/lecturer" , $notification => ROOT."/lecturer/notifications", "profile" => ROOT."/lecturer/viewprofile" , "logout" => ROOT."/lecturer/confirmlogout"]; //pass the menu items here (key is the name of the page, value is the url)
     require_once("../app/views/components/new_navbar.php"); //call the navbar component
-    require_once("../app/views/components/lec_sidebar.php"); //call the sidebar component
+ 
     ?>
     <main class="main-content">
       <div class="container">
