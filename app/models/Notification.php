efd1a2871a32717c59188cc54819a7dabf39be49
Praveen Madushan <prvnmadushan@gmail.com>
2025-04-23 13:52:31 +0530

    notifications

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -13,5 +13,24 @@ class Notification{
         'created_at'
     ];
 
+    public function removeOldNotifications(){
+        $user=$_SESSION['userDetails']->username;
+        $query = "DELETE FROM $this->table WHERE is_read = 1 AND reciptient = :reciptient";
+        $this->query($query, ['reciptient' => $user]);
+    }
+
+    public function updateNotificationState($notification_id){
+        $data=[
+            'notification_id' => $notification_id,
+            'reciptient' => $_SESSION['userDetails']->username
+        ];
+        $query = "UPDATE $this->table SET is_read = 1 WHERE notification_id = :notification_id AND reciptient = :reciptient";
+        $this->query($query, $data);
+    }
+
+        
+
+    
+
 
 }
